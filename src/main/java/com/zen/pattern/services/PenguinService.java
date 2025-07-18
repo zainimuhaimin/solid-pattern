78fc1efab6a58c02f29f726674aacaf99d6fd815
Zaini Muhaimin <zaini.muhaimin@ist.id>
2025-07-18 19:30:22 +0700

    implementasi LSP dengan membuat method yang sama di subclass nya dan merubah logic serta resultnya

--- src/main/java/com/zen/pattern/services/PenguinService.java
+++ src/main/java/com/zen/pattern/services/PenguinService.java
@@ -1,10 +1,25 @@
 package com.zen.pattern.services;
 
 import com.zen.pattern.services.base.Bird;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class PenguinService extends Bird implements SwimableService {
+
+    /**
+     * di subclass ini lo bisa bikin method yang sama dari superclassnya dan bisa lo ubah result bahkan logicnya
+     * @param bird
+     * @return
+     */
+    @Override
+    public String layEggs(String bird) {
+        log.info("penguin has blue eggs");
+        return String.format("penguin has blue eggs");
+    }
+
+
     @Override
     public String swim(String bird) {
         this.layEggs(bird);
